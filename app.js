bb355612ee609431e57674f0a847031e1811f67d
mansipadgelwar <mpadgelwar@gmail.com>
2021-07-26 18:43:04 +0530

    feat: update social links

--- app.js
+++ app.js
@@ -1,43 +1,55 @@
-const form = document.forms[0];
-var purchasePrice = document.querySelector("#purchase-price");
-var stockQuantity = document.querySelector("#stock-quantity");
-var currentPrice = document.querySelector("#current-price");
+var oldPrice = document.querySelector("#purchase-price");
+var quantity = document.querySelector("#stock-quantity");
+var newPrice = document.querySelector("#current-price");
+var output = document.querySelector("#output");
 var btnSubmit = document.querySelector("#btn-submit");
 var profitGIF = "./images/profit.webp";
 var lossGIF = "./images/loss.webp"; 
 
-btnSubmit.addEventListener('click', profitLoss);
-
 
 function profitLoss(e){
     e.preventDefault();
-    purchasePrice = Number(purchasePrice.value);
-    stockQuantity = Number(stockQuantity.value);
-    currentPrice = Number(currentPrice.value);
-    console.log(purchasePrice);
+    purchasePrice = Number(oldPrice.value);
+    stockQuantity = Number(quantity.value);
+    currentPrice = Number(newPrice.value);
     
-if(purchasePrice > 0 || stockQuantity > 0 || currentPrice > 0){
-    if(purchasePrice > currentPrice){
-        const loss = (purchasePrice - currentPrice) * stockQuantity;
-        const percentLoss = (((purchasePrice - currentPrice)*100)/purchasePrice).toFixed(2);
-        output.innerHTML ="You lost " + percentLoss +
-        " %. Your total loss is Rs." + loss;
-        outputGIF.style.display = "block";
-        outputGIF.style.backgroundImage = "url('/images/loss.webp')";
-      }
-    else{
-        const profit = (currentPrice - purchasePrice) * stockQuantity;
-        const percentProfit = (((currentPrice - purchasePrice)*100)/purchasePrice).toFixed(2);
-        output.innerHTML ="You gained " + percentProfit +
-        " %. Your total profit is Rs." + profit;
-        outputGIF.style.display = "block";
-        outputGIF.style.backgroundImage = "url('/images/profit.webp')";
+    if(purchasePrice != "" && stockQuantity != "" && currentPrice != "")
+    {
+       if(purchasePrice > 0 && stockQuantity > 0 && currentPrice > 0)
+       {
+    
+         if(purchasePrice > currentPrice)
+         {
+            const loss = (purchasePrice - currentPrice) * stockQuantity;
+            const percentLoss = (((purchasePrice - currentPrice)*100)/purchasePrice).toFixed(2);
+            output.innerHTML ="You lost " + percentLoss +
+            " %. Your total loss is Rs." + loss;
+            outputGIF.style.display = "block";
+            outputGIF.style.backgroundImage = "url('/images/loss.webp')";
+         }
+         else
+         {
+            const profit = (currentPrice - purchasePrice) * stockQuantity;
+            const percentProfit = (((currentPrice - purchasePrice)*100)/purchasePrice).toFixed(2);
+            output.innerHTML ="You gained " + percentProfit +
+            " %. Your total profit is Rs." + profit;
+            outputGIF.style.display = "block";
+            outputGIF.style.backgroundImage = "url('/images/profit.webp')";
+         }
+    
+        }
+        else
+        {
+        output.innerHTML = "Enter values greater than 0";
+        }
+    }    
+    else
+    {
+    output.innerHTML = "All the fields are mandatory, please enter all the values!";
     
     }
-}
-else{
-    output.innerHTML = "Enter values greater than 0";
-}
 
 
 }
+
+btnSubmit.addEventListener("click", profitLoss);
